b6ae654132ce3f67c8249c671235b238413a3da6
Sean Long <sean.long@octopus.com>
2021-09-20 11:21:48 +1000

    Minimise params displayed on build configuration pages - Reduce a steps param description to minimal subset of relevant information - Add test coverage

--- octopus-server/src/main/java/octopus/teamcity/server/OctopusGenericRunType.java
+++ octopus-server/src/main/java/octopus/teamcity/server/OctopusGenericRunType.java
@@ -1,7 +1,6 @@
 package octopus.teamcity.server;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -33,25 +32,28 @@ public class OctopusGenericRunType extends RunType {
     return OctopusConstants.GENERIC_RUNNER_TYPE;
   }
 
+  @NotNull
   @Override
   public String getDisplayName() {
     return "OctopusDeploy";
   }
 
+  @NotNull
   @Override
   public String getDescription() {
     return "Execute an operation against an OctopusDeploy server";
   }
 
+  @NotNull
   @Override
-  public String describeParameters(final Map<String, String> parameters) {
+  public String describeParameters(@NotNull final Map<String, String> parameters) {
     // NOTE: This is only called once the values in the map have been validated as being "within
     // bounds"
     final CommonStepUserData commonStepUserData = new CommonStepUserData(parameters);
 
     final String stepType = commonStepUserData.getStepType();
     if (commonStepUserData.getStepType().isEmpty()) {
-      return "No build step specified";
+      return "No build step specified\n";
     }
 
     final BuildStepCollection buildStepCollection = new BuildStepCollection();
@@ -62,51 +64,12 @@ public class OctopusGenericRunType extends RunType {
             .findFirst();
 
     if (!buildStep.isPresent()) {
-      return "No build command corresponds to supplied build step name";
+      return "No build command corresponds to supplied build step name\n";
     }
 
-    final StringBuilder builder = new StringBuilder(buildStep.get().getDescription());
-    builder.append("\n");
-    try {
-      final String commonStepDescription = describeCommonParameters(commonStepUserData);
-      builder.append(commonStepDescription);
-      builder.append("\n");
-    } catch (final MalformedURLException e) {
-      return "Failed to parse provided URL - contact octopus support ("
-          + e.getLocalizedMessage()
-          + ")";
-    }
-
-    builder.append(buildStep.get().describeParameters(parameters));
-    return builder.toString();
-  }
-
-  private String describeCommonParameters(final CommonStepUserData commonStepUserData)
-      throws MalformedURLException {
-    final StringBuilder builder = new StringBuilder();
-    builder.append("Server: ");
-    builder.append(commonStepUserData.getServerUrl().toString());
-    builder.append("\n");
-
-    final Optional<String> space = commonStepUserData.getSpaceName();
-    builder.append("Space: ");
-    builder.append(!space.isPresent() ? "<default space>" : space);
-    builder.append("\n");
-
-    if (commonStepUserData.getProxyRequired()) {
-      builder.append("Use Proxy: true\n");
-      builder.append("Proxy Server: ");
-      builder.append(commonStepUserData.getProxyServerUrl());
-      builder.append("\n");
-      builder.append("Username: ");
-      builder.append(commonStepUserData.getProxyUsername());
-      builder.append("\n");
-      builder.append("Password: *****");
-    } else {
-      builder.append("Use Proxy: false\n");
-    }
-
-    return builder.toString();
+    return String.format(
+        "%s\n%s\n",
+        buildStep.get().getDescription(), buildStep.get().describeParameters(parameters));
   }
 
   @Nullable
