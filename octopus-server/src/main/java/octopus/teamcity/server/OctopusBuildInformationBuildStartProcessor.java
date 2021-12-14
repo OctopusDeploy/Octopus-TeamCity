7c9180b506ae71f13813d6a507d15725a6568112
Trent Mohay <trent.mohay@octopus.com>
2021-12-14 14:57:40 +1100

    separate buildInfo and stepvnext start processors

--- octopus-server/src/main/java/octopus/teamcity/server/OctopusBuildInformationBuildStartProcessor.java
+++ octopus-server/src/main/java/octopus/teamcity/server/OctopusBuildInformationBuildStartProcessor.java
@@ -2,87 +2,49 @@ package octopus.teamcity.server;
 
 import java.util.List;
 
+import com.intellij.openapi.diagnostic.Logger;
 import jetbrains.buildServer.ExtensionHolder;
+import jetbrains.buildServer.log.Loggers;
 import jetbrains.buildServer.serverSide.BuildStartContext;
 import jetbrains.buildServer.serverSide.BuildStartContextProcessor;
-import jetbrains.buildServer.serverSide.SProject;
-import jetbrains.buildServer.serverSide.SRunnerContext;
 import jetbrains.buildServer.serverSide.SRunningBuild;
-import jetbrains.buildServer.serverSide.oauth.OAuthConnectionDescriptor;
-import jetbrains.buildServer.serverSide.oauth.OAuthConnectionsManager;
-import jetbrains.buildServer.util.StringUtil;
 import jetbrains.buildServer.vcs.VcsRootInstanceEntry;
-import octopus.teamcity.common.commonstep.CommonStepPropertyNames;
-import octopus.teamcity.server.connection.OctopusConnection;
+import octopus.teamcity.common.OctopusConstants;
 
 public class OctopusBuildInformationBuildStartProcessor implements BuildStartContextProcessor {
 
   private final ExtensionHolder extensionHolder;
-  private final OAuthConnectionsManager oAuthConnectionsManager;
+  private final Logger logger = Loggers.SERVER;
 
-  public OctopusBuildInformationBuildStartProcessor(
-      final ExtensionHolder extensionHolder,
-      final OAuthConnectionsManager oAuthConnectionsManager) {
+  public OctopusBuildInformationBuildStartProcessor(final ExtensionHolder extensionHolder) {
     this.extensionHolder = extensionHolder;
-    this.oAuthConnectionsManager = oAuthConnectionsManager;
   }
 
   @Override
   public void updateParameters(final BuildStartContext buildStartContext) {
-
-    final SRunningBuild build = buildStartContext.getBuild();
-    final List<VcsRootInstanceEntry> vcsRoots = build.getVcsRootEntries();
-
-    if (vcsRoots.size() != 0) {
+    try {
       boolean buildContainsBuildInformationStep =
           buildStartContext.getRunnerContexts().stream()
-              .anyMatch(rc -> rc.getRunType() instanceof OctopusBuildInformationRunType);
+              .anyMatch(
+                  rc -> rc.getRunType().getType().equals(OctopusConstants.METADATA_RUNNER_TYPE));
 
       if (buildContainsBuildInformationStep) {
-        final VcsRootInstanceEntry vcsRoot = vcsRoots.get(0);
-        String vcsType = "Unknown";
-        if (vcsRoot.getVcsName().contains("git")) {
-          vcsType = "Git";
+        final SRunningBuild build = buildStartContext.getBuild();
+        final List<VcsRootInstanceEntry> vcsRoots = build.getVcsRootEntries();
+
+        if (vcsRoots.size() != 0) {
+          final VcsRootInstanceEntry vcsRoot = vcsRoots.get(0);
+          String vcsType = "Unknown";
+          if (vcsRoot.getVcsName().contains("git")) {
+            vcsType = "Git";
+          }
+          buildStartContext.addSharedParameter("octopus_vcstype", vcsType);
         }
-        buildStartContext.addSharedParameter("octopus_vcstype", vcsType);
       }
+    } catch (final Throwable t) {
+      logger.error("Failed to write VCS type into the buildstartContext's shared parameters", t);
+      throw t;
     }
-
-    final String enableStepVnext = System.getProperty("octopus.enable.step.vnext");
-    if (!StringUtil.isEmpty(enableStepVnext) && Boolean.parseBoolean(enableStepVnext)) {
-      insertConnectionPropertiesIntoOctopusBuildSteps(buildStartContext);
-    }
-  }
-
-  private void insertConnectionPropertiesIntoOctopusBuildSteps(
-      final BuildStartContext buildStartContext) {
-    final SProject project = buildStartContext.getBuild().getBuildType().getProject();
-    final List<OAuthConnectionDescriptor> connections =
-        oAuthConnectionsManager.getAvailableConnectionsOfType(project, OctopusConnection.TYPE);
-
-    // For each OctopusGenericBuildStep in the build, find the referenced connection, and copy
-    // parameters into the runnerParams
-    buildStartContext.getRunnerContexts().stream()
-        .filter(rc -> rc.getRunType() instanceof OctopusGenericRunType)
-        .forEach(context -> updateBuildStepWithConnectionProperties(connections, context));
-  }
-
-  private void updateBuildStepWithConnectionProperties(
-      final List<OAuthConnectionDescriptor> connections, final SRunnerContext context) {
-    final String connectionId = context.getParameters().get(CommonStepPropertyNames.CONNECTION_ID);
-
-    connections.stream()
-        .filter(c -> c.getId().equals(connectionId))
-        .findFirst()
-        .orElseThrow(
-            () ->
-                new IllegalArgumentException(
-                    "No Octopus connection '"
-                        + connectionId
-                        + "' exists for the current "
-                        + "project"))
-        .getParameters()
-        .forEach(context::addRunnerParameter);
   }
 
   public void register() {
